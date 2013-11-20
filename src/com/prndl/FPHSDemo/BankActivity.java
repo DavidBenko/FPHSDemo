140271b1a6d81d10ac34c3fabd0ae6b116586768
David Benko <dbenko@prndl.us>
2013-11-20 17:57:11 -0500

    Generated Android Project

---
+++ src/com/prndl/FPHSDemo/BankActivity.java
@@ -0,0 +1,22 @@
+package com.prndl.FPHSDemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BankActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bank);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.bank, menu);
+		return true;
+	}
+
+}
